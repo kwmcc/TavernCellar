40bdfa0fd5d248738f2c477d9afb16f9dd687c55
Nicholas Lampert <nmlamper@ucsc.edu>
2016-04-24 16:38:40 -0700

    Submit has functional uploads

--- tavernCellar.py
+++ tavernCellar.py
@@ -1,14 +1,24 @@
+import os
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from flask.ext.script import Manager
 from flask.ext.moment import Moment
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = 'uploads/'
+ALLOWED_EXTENSIONS = set(['pdf'])
 
 app = Flask(__name__)
+app.debug = True
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 manager = Manager(app)
 moment = Moment(app)
 
-
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+	
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -39,8 +49,14 @@ def browse():
     return render_template('browse.html')
 
 
-@app.route('/submit')
+@app.route('/submit', methods=['GET', 'POST'])
 def submit():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('srd', filename=filename))
     return render_template('submit.html')
 
 
