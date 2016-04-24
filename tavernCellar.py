1627a1457e0e4aa8e71dc932ddd808eca4ca6a46
Nicholas Lampert <nmlamper@ucsc.edu>
2016-04-24 14:30:14 -0700

    Removed BootStrap

--- tavernCellar.py
+++ tavernCellar.py
@@ -1,13 +1,11 @@
 from datetime import datetime
 from flask import Flask, render_template
 from flask.ext.script import Manager
-from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 
 app = Flask(__name__)
 
 manager = Manager(app)
-bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
