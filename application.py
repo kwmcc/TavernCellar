1ad55edb2b061759acd1085f317b13bf7c0feab4
kwmcc <kmccotte@ucsc.edu>
2016-05-23 20:07:01 -0700

    remade environment

--- application.py
+++ application.py
@@ -1,6 +1,12 @@
-from flask import Flask
+# application.py - for Elastic Beanstalk to easily start the server,
+# this file should do essentially what "python manage.py runserver" does.
 
-application = app = Flask(__name__)
+import os
+from app import create_app, db
+
+application = app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app.secret_key= 'Secret'
 
 if __name__ == "__main__":
+    application.debug = True
     application.run(host='0.0.0.0')
