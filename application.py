1c595a01db4dbeee9b7124a3f58dd60db6370b5c
kwmcc <kmccotte@ucsc.edu>
2016-05-24 19:12:44 -0700

    debug off

--- application.py
+++ application.py
@@ -8,5 +8,5 @@ application = app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 app.secret_key= 'Secret'
 
 if __name__ == "__main__":
-    application.debug = True
+    application.debug = False
     application.run(host='0.0.0.0')
