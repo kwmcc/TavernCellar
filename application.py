8102b5ee2280521787d03f9e3e11d164f1beee0d
kwmcc <kmccotte@ucsc.edu>
2016-05-30 14:15:22 -0700

    drop on deploy

--- application.py
+++ application.py
@@ -20,7 +20,7 @@ app.secret_key= 'Secret'
 
 if __name__ == "__main__":
     with app.app_context():
-        #db.drop_all()
+        db.drop_all()
         db.create_all()
     application.debug = False
     application.run(host='0.0.0.0')
