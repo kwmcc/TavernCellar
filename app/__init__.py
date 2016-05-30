715e55ccc99a891b2aac501afd03c30094505c0c
kwmcc <kmccotte@ucsc.edu>
2016-05-30 13:46:07 -0700

    database creation
    database should now properly instantiate on deployment
    cleaned up init and config

--- app/__init__.py
+++ app/__init__.py
@@ -27,6 +27,7 @@ def create_app(config_name):
     login_manager.init_app(app)
     material.init_app(app)
     mail.init_app(app)
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
--- application.py
+++ application.py
@@ -3,10 +3,24 @@
 
 import os
 from app import create_app, db
+#from migrate.versioning import api
+#from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO
 
-application = app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+application = app = create_app(os.getenv('FLASK_CONFIG') or 'production')
 app.secret_key= 'Secret'
 
+# def db_create():
+#     db.create_all()
+#     if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+#         api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+#         api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+#     else:
+#         api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
+
+
 if __name__ == "__main__":
+    with app.app_context():
+        #db.drop_all()
+        db.create_all()
     application.debug = False
     application.run(host='0.0.0.0')
