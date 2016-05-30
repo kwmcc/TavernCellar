674652cc24fc0e3c96982c0c5b42e1bc0716a2df
kwmcc <kmccotte@ucsc.edu>
2016-05-30 15:16:05 -0700

    debug off

--- application.py
+++ application.py
@@ -22,5 +22,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.drop_all()
         db.create_all()
-    application.debug = True
+    application.debug = False
     application.run(host='0.0.0.0')
